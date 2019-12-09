7a9cb94ae7c6024452775fccef86fc389590279d
CarstenLeonhardt <24877208+CarstenLeonhardt@users.noreply.github.com>
2019-12-09 21:33:49 +0100

    added ArmedAway ArmedHome

--- BinarySwitchSensor/BinarySwitchSensor.ino
+++ BinarySwitchSensor/BinarySwitchSensor.ino
@@ -25,16 +25,42 @@
  * digitial I/O pin 3 (BUTTON_PIN below) and GND.
  * http://www.mysensors.org/build/binary
  */
-
+ 
+/*
+ * HW: Arduino promini 8mhz
+ * 3 * digital input from Alarmsystem Armed / Siren active / Alarm completly armed
+ */
 
 // Enable debug prints to serial monitor
-#define MY_DEBUG 
+//#define MY_DEBUG 
 
 // Enable and select radio type attached
 #define MY_RADIO_NRF24
 //#define MY_RADIO_RFM69
-#define MY_NODE_ID 22 // Sets a static id for a node //22 AlarmEnabledSensor
+
 #define MY_RF24_PA_LEVEL RF24_PA_MAX // Max tx power
+
+#if defined(ARDUINO_AVR_NANO)
+  //Only needed for Keywish NanoExpansionBoard with screw terminals
+  #define MY_RF24_CE_PIN 10
+  #define MY_RF24_CS_PIN 9
+  #define MY_NODE_ID 24 // Sets a static id for a node 
+  #define CHILD_ARMED_ID 1
+  #define CHILD_SIREN_ID 2
+  #define CHILD_ARMED_AWAY_ID 3 //Away
+  #define CHILD_ARMED_HOME_ID 4 //Home
+#elif defined(ARDUINO_AVR_PRO)
+  // Pro Mini assignments
+  #define CHILD_ARMED_ID 3
+  #define CHILD_SIREN_ID 4
+  #define CHILD_ARMED_AWAY_ID 5 //Away
+  #define CHILD_ARMED_HOME_ID 6 //Home
+  #define MY_NODE_ID 22 // Sets a static id for a node //22 AlarmEnabled
+#else
+  #error Unsupported board selection.
+#endif
+
+
 
 //Enable Repeater function
 #define MY_REPEATER_FEATURE
@@ -44,23 +70,26 @@
 #include <Bounce2.h>
 
 
-#define CHILD_ARMED_ID 3
-#define CHILD_SIREN_ID 4
+
 #define ALARM_ARMED_PIN  3  // Alarm Armed Activated
 #define ALARM_SIREN_PIN  4  // Alarm Siren Activated
-
-//Send direct node messages
-#define ALARM_NODE_1_ID 49 //desktop node
-#define CHILD_ID_LED 2
+#define ALARM_ARMED_COMPLETE_PIN  5  // Alarm Completly Armed Activated
 
 
 Bounce debouncerA = Bounce(); 
 Bounce debouncerS = Bounce(); 
+Bounce debouncerC = Bounce(); 
+
 int oldValueA=-1;
 int oldValueS=-1;
-uint32_t prevMillis;
+int oldValueC=-1;
+int oldValueP=-1;
+
 int ValueA = 0;
 int ValueS = 0;
+int ValueC = 0;
+int ValueP = 0;
+uint32_t prevMillis;
 
 // Sleep time between sensor updates (in milliseconds)
 static const uint64_t UPDATE_INTERVAL = 60000;
@@ -68,6 +97,8 @@ static const uint64_t UPDATE_INTERVAL = 60000;
 // Change to V_LIGHT if you use S_LIGHT in presentation below
 MyMessage msgA(CHILD_ARMED_ID, V_ARMED);
 MyMessage msgS(CHILD_SIREN_ID, V_TRIPPED);
+MyMessage msgC(CHILD_ARMED_AWAY_ID, V_ARMED);
+MyMessage msgP(CHILD_ARMED_HOME_ID, V_ARMED);
 
 
 void setup()  
@@ -75,26 +106,65 @@ void setup()
   // Setup the button
   pinMode(ALARM_ARMED_PIN,INPUT);
   pinMode(ALARM_SIREN_PIN,INPUT);
+  pinMode(ALARM_ARMED_COMPLETE_PIN,INPUT);
+  
   // Activate internal pull-up
   digitalWrite(ALARM_ARMED_PIN,HIGH);
   digitalWrite(ALARM_SIREN_PIN,HIGH);
+  digitalWrite(ALARM_ARMED_COMPLETE_PIN,HIGH);
   
   // After setting up the button, setup debouncer
   debouncerA.attach(ALARM_ARMED_PIN);
-  debouncerA.interval(5);
+  debouncerA.interval(100);
   debouncerS.attach(ALARM_SIREN_PIN);
-  debouncerS.interval(5);
+  debouncerS.interval(100);  
+  debouncerC.attach(ALARM_ARMED_COMPLETE_PIN);
+  debouncerC.interval(100);
   
+}
+//Retransmit present if it was not acked
+void represent(uint8_t childSensorId, uint8_t sensorType, const char *description, int repeats)
+{
+  int repeat = 1;
+  int repeatdelay = 0;
+  boolean sendOK = false;
+
+  while ((sendOK == false) and (repeat < repeats)) 
+  {
+    if (present(childSensorId, sensorType, description, false)) 
+    {
+      sendOK = true;
+    } 
+    else 
+    {
+      sendOK = false;
+      Serial.print(F("Unable to transmit present(), retry count: "));
+      Serial.println(repeat);
+      repeatdelay += 250;
+    } 
+    repeat++; 
+    delay(repeatdelay);
+  }
 }
 
 void presentation() 
 {
   // Register binary input sensor to gw (they will be created as child devices)
+  sendSketchInfo("FrontiAlarm Status", "1.7");
+  Serial.println(F("Presentation() "));
+  delay(1000);
   // You can use S_DOOR, S_MOTION or S_LIGHT here depending on your usage. 
   // If S_LIGHT is used, remember to update variable type you send in. See "msg" above.
-  present(CHILD_ARMED_ID, S_DOOR);  
-  present(CHILD_SIREN_ID, S_DOOR);  
+  represent(CHILD_ARMED_AWAY_ID, S_DOOR, "ArmedComplete", 10);  
+  represent(CHILD_ARMED_HOME_ID, S_DOOR, "ArmedPartly", 10);  
+  represent(CHILD_ARMED_ID, S_DOOR, "Armed", 10);  
+  represent(CHILD_SIREN_ID, S_DOOR, "Siren", 10);  
+  Serial.println(F("Presentation() Done "));
 }
+
+
+
+
 
 
 //Retransmit message if it was not acked
@@ -113,7 +183,7 @@ void resend(MyMessage &msg, int repeats)
     else 
     {
       sendOK = false;
-      Serial.print("Unable to transmit, retry count: ");
+      Serial.print(F("Unable to transmit, retry count: "));
       Serial.println(repeat);
       repeatdelay += 250;
     } 
@@ -122,80 +192,122 @@ void resend(MyMessage &msg, int repeats)
   }
 }
 
+int GetPinState(Bounce &debouncer, int &oldValue, MyMessage &msg, int repeats, String pinname, bool Verbose)
+{
+  int value = 0;
+  int retval = oldValue==HIGH ? 0 : 1;
+  debouncer.update();
+  // Get the update value
+  value = debouncer.read();
+
+  if (value != oldValue) 
+  {
+    if (Verbose)
+    {
+      Serial.print(F("Pin "));
+      Serial.print(pinname);
+      Serial.print(F(" state changed, was: "));
+      Serial.print(oldValue);
+      Serial.print(F(", is now: "));
+      Serial.print(value);
+    }
+    // Send in the new value
+    retval = value==HIGH ? 0 : 1;
+    if (Verbose)
+    {
+      Serial.print(F(" - State is now: "));
+      Serial.println(retval);
+    }
+    resend((msg.set(retval)), repeats);
+    oldValue = value;
+  }
+  return retval;
+}
+
+int GetArmedHome(int Armed, int ArmedAway, int &oldValue, MyMessage &msg, int repeats, String pinname, bool Verbose)
+{
+  int retval = 0;
+   // Report if the alarm is Armed Home
+  if (Armed == 0)
+  {
+    if  (ArmedAway == 0) 
+    { //Armed Away activated alarm
+      retval = 0;
+    }
+    else if (ArmedAway == 1) 
+    { //Armed Home activated alarm
+      retval = 1;
+    }
+  }
+  else
+  {
+      retval = 0;
+  }
+  
+  if (retval != oldValue)
+  {  
+    oldValue = retval;
+    if (Verbose)
+    {
+      Serial.print(pinname);
+      Serial.print(F(" Alarm Home Armed, was: "));
+      Serial.print(oldValue);
+      Serial.print(F(", is now: "));
+      Serial.println(retval);
+    }
+    resend((msg.set(retval)), repeats);
+  }
+  return retval;
+}
+
+void ForcedUpdate(int txvalue, MyMessage &msg, int repeats, String pinname, bool Verbose)
+{
+  if (Verbose)
+  {
+    Serial.print(F("Retransmit value ")); 
+    Serial.print(pinname);
+    Serial.print(F(" : "));
+    Serial.println(txvalue);
+  }
+  resend((msg.set(txvalue)), repeats);
+  delay(200);
+}
+
 //  Check if digital input has changed and send in new value
 void loop() 
 {
-  debouncerA.update();
-  // Get the update value
-  int value = debouncerA.read();
- 
-  if (value != oldValueA) 
-  {
-     Serial.print("Pin ARMED state changed, was: ");
-     Serial.print(oldValueA);
-     Serial.print(", is now: ");
-     Serial.println(value);
-   
-     // Send in the new value
-     ValueA = value==HIGH ? 0 : 1;
-     Serial.print("ValueA is now: ");
-     Serial.println(ValueA);
-    
-     resend((msgA.set(ValueA)), 10);
-//     resend((msgA.set(value==HIGH ? 0 : 1)), 10);
-     //send(msgA.set(value==HIGH ? 0 : 1));
-     //Send data direct to sister node
-//     Serial.println("Send data to sisteer node: ");
-//     send(msgA.setDestination(ALARM_NODE_1_ID).setSensor(CHILD_ID_LED).set(value==HIGH ? 0 : 1));
-     oldValueA = value;
-  }
+  
+  // Alarm Armed
+  ValueA = GetPinState(debouncerA, oldValueA, msgA, 10, F("ARMED"), true);
 
-  debouncerS.update();
-  // Get the update value
-  value = debouncerS.read();
- 
-  if (value != oldValueS) 
-  {
-     delay(200);
-     Serial.print("Pin SIREN state changed, was: ");
-     Serial.print(oldValueS);
-     Serial.print(", is now: ");
-     Serial.println(value);
-   
-     // Send in the new value
-     //send(msgS.set(value==HIGH ? 0 : 1));
-     ValueS = value==HIGH ? 0 : 1;
-     Serial.print("ValueS is now: ");
-     Serial.println(ValueS);
+  // Siren active
+  ValueS = GetPinState(debouncerS, oldValueS, msgS, 10, F("SIREN"), true);
 
-     //resend((msgS.set(value==HIGH ? 0 : 1)), 10);
-     resend((msgS.set(ValueS)), 10);
-     oldValueS = value;
-  }
+  // Alarm completly armed  - Armed Away
+  ValueC = GetPinState(debouncerC, oldValueC, msgC, 10, F("ArmedAway"), true);
 
+  // Report if the alarm is Partly Armed - Armed Home
+  ValueP = GetArmedHome(oldValueA, oldValueC, oldValueP, msgP, 10, F("ArmedHome"), true);
 
   //Send digital input state every minute
   if (millis() - prevMillis >= UPDATE_INTERVAL) 
   {
+      //Serial.print("ValueA is : ");
+      //Serial.println(ValueA);
+      //Serial.print("ValueC is : ");
+      //Serial.println(ValueC);
+      //Serial.print("ValueS is : ");
+      //Serial.println(ValueS);
+      //Serial.print("ValueP is : ");
+      //Serial.println(ValueP);
       prevMillis += UPDATE_INTERVAL;
-      Serial.print("Retransmit value Armed: ");
-      Serial.println(ValueA);
-      resend((msgS.set(ValueA)), 10);
-      //resend((msgA.set(oldValueA==HIGH ? 0 : 1)), 10);
-      //send(msgA.set(oldValueA==HIGH ? 0 : 1));
-
-      delay(200);
-
-      Serial.print("Retransmit value Siren: ");
-      Serial.println(ValueS);
-      resend((msgS.set(ValueS)), 10);
-      //resend((msgS.set(oldValueS==HIGH ? 0 : 1)), 10);
-      //send(msgS.set(oldValueS==HIGH ? 0 : 1));
-  
-    //  delay(200);
-      
-      //Send data direct to sister node
-//      Serial.println("Send data to sisteer node: ");
-//      send(msgA.setDestination(ALARM_NODE_1_ID).setSensor(CHILD_ID_LED).set(oldValueA==HIGH ? 0 : 1));
+      //Serial.println(F("ForcedUpdate Armed"));
+      ForcedUpdate(ValueA, msgA, 10, "Armed" , true);
+      //Serial.println(F("ForcedUpdate Away"));
+      ForcedUpdate(ValueC, msgC, 10, "Alarm Away Armed" , true);
+      //Serial.println(F("ForcedUpdate Siren"));
+      ForcedUpdate(ValueS, msgS, 10, "Siren" , true);
+      //Serial.println(F("ForcedUpdate Home"));
+      ForcedUpdate(ValueP, msgP, 10, "Alarm Home Armed" , true);
   }
 } 
